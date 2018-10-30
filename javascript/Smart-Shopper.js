4eb5b1695a389f1de56f7a4ded988b48f7b5018c
Adrian Herrmann <adrianherrmann@adrians-mbp-3.attlocal.net>
2018-10-30 02:23:55 -0700

    Added skeleton code with plenty of working links and placeholders.

---
+++ javascript/Smart-Shopper.js
@@ -0,0 +1,34 @@
+/* When clicking on + Add New */
+$(".add-new").click(function() {
+
+});
+
+$(".btn-back").click(function(e) {
+  window.history.back();
+});
+
+$(".btn-back-homepage").click(function(e) {
+  window.location="Home-Screen.html";
+});
+
+$(".to-scan-search-recipes").click(function(e) {
+  window.location="Scan-Search-Recipes.html";
+});
+
+
+$(".to-scan").click(function(e) {
+  window.location="Scan.html";
+});
+
+$(".to-search").click(function(e) {
+  window.location="Search.html";
+});
+
+$(".to-recipes").click(function(e) {
+  window.location="Recipes.html";
+});
+
+/* Go to results page */
+$(".to-results").click(function(e) {
+  window.location="Results.html";
+});
