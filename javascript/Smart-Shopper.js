d387217d51c68d2921f722e244587c238a734eac
Young <yecho@ucsd.edu>
2018-11-08 12:24:57 -0800

    List change and index to create user

--- javascript/Smart-Shopper.js
+++ javascript/Smart-Shopper.js
@@ -1,6 +1,28 @@
-/* When clicking on + Add New */
-$(".add-new").click(function() {
+/* When clicking on delete for creating a list*/
+$(document).on("click", ".delete", function(e) {
+  $(this).closest('div').remove();
+});
 
+$(".close").click(function(e) {
+  window.location="Scan-Search-Recipes.html";
+});
+
+/* When clicking on + Add New */
+$(document).on("click", ".add-new", function() {
+  $(this).replaceWith("<input type='text' class='restriction-text' onfocus=" +
+                      "'this.value=&quot;&quot;' value='Enter Restriction...'>");
+});
+
+/* Add restriction when user presses enter */
+$(document).on("keypress", ".restriction-text", function (e) {
+  var key = e.which;
+  if(key == 13)  // the enter key code
+  {
+    $(this).replaceWith("<div><p class='restriction'><font size='5'>\xa0\xa0" +
+                        "<button class='delete'>-</button>\xa0\xa0" +
+                        $(this).val() +
+                        "</font></p></div><button class='add-new'>+ Add</button>");
+  }
 });
 
 $(".btn-back").click(function(e) {
@@ -8,13 +30,12 @@ $(".btn-back").click(function(e) {
 });
 
 $(".btn-back-homepage").click(function(e) {
-  window.location="Home-Screen.html";
+  window.location="index.html";
 });
 
 $(".to-scan-search-recipes").click(function(e) {
   window.location="Scan-Search-Recipes.html";
 });
-
 
 $(".to-scan").click(function(e) {
   window.location="Scan.html";
@@ -32,3 +53,18 @@ $(".to-recipes").click(function(e) {
 $(".to-results").click(function(e) {
   window.location="Results.html";
 });
+
+/* A bit sloppy, but couldn't get the for loop version to work */
+function timeScan() {
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING.</h2>";}, 500);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING..</h2>";}, 1000);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING...</h2>";}, 1500);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING.</h2>";}, 2000);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING..</h2>";}, 2500);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING...</h2>";}, 3000);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING.</h2>";}, 3500);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING..</h2>";}, 4000);
+  setTimeout(function(){document.getElementById("scanner").innerHTML="<h2 id='scanner'>SCANNING...</h2>";}, 4500);
+
+  setTimeout(function(){window.location="Results.html";}, 5000);
+}
